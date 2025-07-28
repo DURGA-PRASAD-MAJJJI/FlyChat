ed1312be078c0589f48bedca1a4250ef9dfe3d4e
MAJJI DURGA PRASAD <durgaprasadmajji65@gmail.com>
2025-07-28 14:07:47 +0530

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-
-const BASE_URL = import.meta.env.MODE === "development" ? "https://fly-chat-be.vercel.app/api" : "https://fly-chat-be.vercel.app/api";
-
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+ const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true, // ✅ required to send cookies cross-origin
 });
+
+export default axiosInstance;
